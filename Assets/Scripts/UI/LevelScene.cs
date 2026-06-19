3ded1e72812f60311f88e70946cf7a992456845b
Lieuquichuong2005 <quichuong313@gmail.com>
2026-06-19 21:30:29 +0700

    Add LevelView. LevelScene

---
+++ Assets/Scripts/UI/LevelScene.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelScene : MonoBehaviour
+{
+    [SerializeField] private Image _firePower;
+
+    [SerializeField] private Image _stamina;
+
+    [SerializeField] private TextMeshProUGUI _fireAngle;
+}
