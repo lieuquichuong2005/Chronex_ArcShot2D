923b58c588cbec4789eee5c0de177b58b262d116
Lieuquichuong2005 <quichuong313@gmail.com>
2026-07-12 05:12:54 +0700

    Add Audio Manager, Config

---
+++ Assets/Scripts/Configs/Audio/Audio.cs
@@ -0,0 +1,5 @@
+public enum Audio
+{
+    None,
+    SFX_Click,
+}
---
+++ Assets/Scripts/Configs/Audio/AudioLibrary.cs
@@ -0,0 +1,10 @@
+using QuiChuong2005.Framework.Services.Audio.Library;
+using UnityEngine;
+
+namespace QuiChuong2005.Framework.Services.Audio
+{
+    [CreateAssetMenu(menuName = "QuiChuong2005/Manager/Audio", fileName = "AudioManager")]
+    public class AudioLibrary : AudioLibrarySO<global::Audio>
+    {
+    }
+}
