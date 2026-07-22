1fa27db52e8612bea185605992387ebe335e0380
Lieuquichuong2005 <quichuong313@gmail.com>
2026-07-23 00:15:17 +0700

    Update ResultScene UI

---
+++ Assets/Scripts/UI/Scene/ResultScene.cs
@@ -0,0 +1,57 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultScene : MonoBehaviour
+{
+    [SerializeField]
+    private Image _backgroundImage;
+
+    [SerializeField]
+    private Image _titleImage;
+
+    [SerializeField]
+    private TextMeshProUGUI _playerName;
+
+    [SerializeField]
+    private TextMeshProUGUI _damageDealtText;
+
+    [SerializeField]
+    private TextMeshProUGUI _matchDurationText;
+
+    [SerializeField]
+    private TextMeshProUGUI _damageTakenText;
+
+    [SerializeField]
+    private TextMeshProUGUI _accuracyText;
+
+    [SerializeField]
+    private TextMeshProUGUI _turnsText;
+
+    [SerializeField]
+    private TextMeshProUGUI _critText;
+
+    [SerializeField]
+    private TextMeshProUGUI _coinBonusText;
+
+    [SerializeField]
+    private TextMeshProUGUI _expBonusText;
+
+    [SerializeField]
+    private TextMeshProUGUI _levelShapeText;
+
+    [SerializeField]
+    private TextMeshProUGUI _levelText;
+
+    [SerializeField]
+    private Image _expProcess;
+
+    [SerializeField]
+    private TextMeshProUGUI _expProcessText;
+
+    [SerializeField]
+    private TextMeshProUGUI _expBonus;
+
+    [SerializeField]
+    private Button _backToMenuButton;
+}
